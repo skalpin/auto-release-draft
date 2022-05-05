4c6cc025609b3f17ef291494d18a21e2a5cecc52
Stephen Kalpin <Steve.Kalpin@genmills.com>
2022-05-05 15:55:57 -0500

    Adding semver

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as event from './event'
+import * as version from './version'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const tag = event.getCreatedTag()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (tag && version.isSemVer(tag)) {
+      // TODO
+    }
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setOutput('release-url', 'https://example.com')
+  } catch (error:any) {
+    core.setFailed(error.message)
   }
 }
 
