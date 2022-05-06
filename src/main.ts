b1d386bdc4b0fd01470d5dc675e646272ee4050d
Stephen Kalpin <Steve.Kalpin@genmills.com>
2022-05-06 10:09:17 -0500

    correcting branch name

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ async function run(): Promise<void> {
 
     if (tag && version.isSemVer(tag)) {
       const changelog = await git.getChangesIntroducedByTag(tag)
-      
       releaseUrl = await github.createReleaseDraft(tag, token, changelog)
     }
 
